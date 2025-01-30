a1c3275fbbfe80f54660397706eb941260f1dc0b
ArthurRFr <arthur_fronza2011@hotmail.com>
2025-01-30 08:51:51 -0300

    feat: remove unused error treatment

--- api/src/modules/event/controllers/event.controller.ts
+++ api/src/modules/event/controllers/event.controller.ts
@@ -1,4 +1,5 @@
 import { CreateEventDto } from '@modules/event/dtos/create-event.dtos';
+import { AppError } from '@shared/errors/app.error';
 import { Request, Response } from 'express';
 import { constants } from 'http2';
 import { Inject, Service } from 'typedi';
@@ -22,96 +23,50 @@ export class EventController {
   ) {}
 
   async list(req: Request, res: Response): Promise<Response> {
-    try {
-      const result = await this.listEventsUsecase.execute();
+    const result = await this.listEventsUsecase.execute();
 
-      return res.status(constants.HTTP_STATUS_OK).json(result);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(
-          error?.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        )
-        .json({ message: error?.message || 'internal server error' });
-    }
+    return res.status(constants.HTTP_STATUS_OK).json(result);
   }
 
   async create(req: Request, res: Response): Promise<Response> {
-    try {
-      const { name, odds }: CreateEventDto = req.body;
-      const result = await this.createEventUsecase.execute(name, odds);
-      return res.status(constants.HTTP_STATUS_CREATED).json(result);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(
-          error?.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        )
-        .json({ message: error?.message || 'internal server error' });
-    }
+    const { name, odds }: CreateEventDto = req.body;
+    const result = await this.createEventUsecase.execute(name, odds);
+    return res.status(constants.HTTP_STATUS_CREATED).json(result);
   }
 
   async update(req: Request, res: Response): Promise<Response> {
-    try {
-      const { id } = req.params;
+    const { id } = req.params;
 
-      if (!id) {
-        return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-          message: 'id is required',
-        });
-      }
-
-      const parsedId = Number(id);
-      if (Number.isNaN(parsedId)) {
-        return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-          message: 'id is invalid',
-        });
-      }
-
-      const { name, odds }: UpdateEventDto = req.body;
-
-      const result = await this.updateEventUsecase.execute(
-        parsedId,
-        name,
-        odds
-      );
-
-      return res.status(constants.HTTP_STATUS_OK).json(result);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(
-          error?.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        )
-        .json({ message: error?.message || 'internal server error' });
+    if (!id) {
+      throw new AppError('id is required', constants.HTTP_STATUS_BAD_REQUEST);
     }
+
+    const parsedId = Number(id);
+    if (Number.isNaN(parsedId)) {
+      throw new AppError('id is invalid', constants.HTTP_STATUS_BAD_REQUEST);
+    }
+
+    const { name, odds }: UpdateEventDto = req.body;
+
+    const result = await this.updateEventUsecase.execute(parsedId, name, odds);
+
+    return res.status(constants.HTTP_STATUS_OK).json(result);
   }
 
   async delete(req: Request, res: Response): Promise<Response> {
-    try {
-      const { id } = req.params;
+    const { id } = req.params;
 
-      if (!id) {
-        return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-          message: 'id is required',
-        });
-      }
-
-      const parsedId = Number(id);
-      if (Number.isNaN(parsedId)) {
-        return res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
-          message: 'id is invalid',
-        });
-      }
-
-      await this.deleteEventUsecase.execute(parsedId);
-
-      return res.status(constants.HTTP_STATUS_OK).send();
-    } catch (error) {
-      console.error(error);
-      return res
-        .status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-        .json({ message: 'internal server error' });
+    if (!id) {
+      throw new AppError('id is required', constants.HTTP_STATUS_BAD_REQUEST);
     }
+
+    const parsedId = Number(id);
+    if (Number.isNaN(parsedId)) {
+      throw new AppError('id is invalid', constants.HTTP_STATUS_BAD_REQUEST);
+    }
+
+    await this.deleteEventUsecase.execute(parsedId);
+
+    return res.status(constants.HTTP_STATUS_OK).send();
   }
 }
--- api/src/modules/user/controllers/user.controller.ts
+++ api/src/modules/user/controllers/user.controller.ts
@@ -16,32 +16,14 @@ export class UserController {
   ) {}
 
   async create(req: Request, res: Response): Promise<Response> {
-    try {
-      const { username, password }: CreateUserDto = req.body;
-      const result = await this.createUserUsecase.execute(username, password);
-      return res.status(constants.HTTP_STATUS_CREATED).json(result);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(
-          error?.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        )
-        .json({ message: error?.message || 'internal server error' });
-    }
+    const { username, password }: CreateUserDto = req.body;
+    const result = await this.createUserUsecase.execute(username, password);
+    return res.status(constants.HTTP_STATUS_CREATED).json(result);
   }
 
   async login(req: Request, res: Response): Promise<Response> {
-    try {
-      const { username, password }: LoginUserDto = req.body;
-      const result = await this.loginUserUsecase.execute(username, password);
-      return res.status(constants.HTTP_STATUS_OK).json(result);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(
-          error?.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        )
-        .json({ message: error?.message || 'internal server error' });
-    }
+    const { username, password }: LoginUserDto = req.body;
+    const result = await this.loginUserUsecase.execute(username, password);
+    return res.status(constants.HTTP_STATUS_OK).json(result);
   }
 }
