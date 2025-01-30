7dc92af3797c0a0eb28d9dc4c1f57041f99d5928
ArthurRFr <arthur_fronza2011@hotmail.com>
2025-01-30 19:00:47 -0300

    fix: missing step on migration

--- api/src/shared/infra/typeorm/migrations/1738236554800-uuid-generation.ts
+++ api/src/shared/infra/typeorm/migrations/1738236554800-uuid-generation.ts
@@ -3,6 +3,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class UuidGeneration1738236554800 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
+			CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+		`);
+    await queryRunner.query(`
 				ALTER TABLE "users" 
 				ALTER COLUMN "user_id" SET DEFAULT uuid_generate_v4()
 			`);
@@ -12,6 +15,9 @@ export class UuidGeneration1738236554800 implements MigrationInterface {
     await queryRunner.query(`
 				ALTER TABLE "users" 
 				ALTER COLUMN "user_id" DROP DEFAULT
+			`);
+    await queryRunner.query(`
+				DROP EXTENSION IF EXISTS "uuid-ossp";
 			`);
   }
 }
